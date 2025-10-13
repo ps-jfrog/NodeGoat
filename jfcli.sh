634532d2ac8d790a1319976604253c511c5db0f3
Krishna Manchikalapudi <krishnam@jfrog.com>
2025-10-13 13:06:52 -0700

    check for remote repo

--- jfcli.sh
+++ jfcli.sh
@@ -1,21 +1,21 @@
-export JF_HOST="psazuse.jfrog.io"  JFROG_CLI_LOG_LEVEL="DEBUG" RT_REPO="cg-lab-npm" # "curation-blocked-npm-virtual"  # "curation-blocked-npm-remote" # "curation-blocked-npm-local"
+export JF_HOST="psazuse.jfrog.io"  JFROG_CLI_LOG_LEVEL="DEBUG" RT_VIRTUAL="cg-lab-npm"  RT_REMOTE="cg-lab-npm-remote"
 
 # Curation Policy Name: cg-lab-npm
 
 export JF_RT_URL="https://${JF_HOST}" BUILD_NAME="nodegoat" BUILD_ID="cmd.$(date '+%Y-%m-%d-%H-%M')" 
 
 echo " JF_RT_URL: $JF_RT_URL \n JFROG_CLI_LOG_LEVEL: $JFROG_CLI_LOG_LEVEL \n "
-echo " BUILD_NAME: $BUILD_NAME \n BUILD_ID: $BUILD_ID \n RT_REPO: $RT_REPO"
+echo " BUILD_NAME: $BUILD_NAME \n BUILD_ID: $BUILD_ID \n RT_REPO: $RT_VIRTUAL"
 
 rm -rf node_modules package-lock.json
 echo "NPM ver $(npm -v)"
 echo "Node ver $(node -v) \n\n"
 
-jf npmc --repo-resolve=${RT_REPO} --repo-deploy=${RT_REPO}
+jf npmc --repo-resolve=${RT_VIRTUAL} --repo-deploy=${RT_VIRTUAL}
 
 jf ca --format=table --threads=100 
 
-CURL_URL="${JF_RT_URL}/xray/ui/curation/waiver_requests?pkg_type=PyPI&status=pending&num_of_rows=10"
+CURL_URL="${JF_RT_URL}/xray/ui/curation/waiver_requests?pkg_type=npm&status=pending&num_of_rows=10"
 RESP_JSON="WAIVER_PENDING_RESP-${BUILD_ID}.json"
 
 WAIVER_PENDING_RESP=$(curl "${CURL_URL}" -H "Authorization: Bearer ${JF_ACCESS_TOKEN}")
@@ -30,17 +30,27 @@ JSON_FILE="${RESP_JSON}"
 # jq -c '.data[] | {id, pkg_name, pkg_version}' "$JSON_FILE" | while read -r item; do
 
 jq -c '.data[]' "$JSON_FILE" | while read -r item; do
-    waiver_id=$(echo "$item" | jq -r '.id')
-    pkg_name=$(echo "$item" | jq -r '.pkg_name')
-    pkg_version=$(echo "$item" | jq -r '.pkg_version')
+  waiver_id=$(echo "$item" | jq -r '.id')
+  repo_key=$(echo "$item" | jq -r '.repo_key')
+  pkg_name=$(echo "$item" | jq -r '.pkg_name')
+  pkg_version=$(echo "$item" | jq -r '.pkg_version')
 
-    decision_owners=$(echo "$item" | jq -r '.decision_owners | join(", ")')
+  repo_match=$(echo "$item" | jq -r --arg repo "${RT_REMOTE}" '.policies[] | select(.repo_include | index($repo))')
+  echo $repo_match
+  if [[ -z "$repo_match" ]]; then
+    continue  # skip this item if repo not included
+  fi
 
-    latest_request=$(echo "$item" | jq -r '.requesters | sort_by(.requested_at) | last')
-    requested_at=$(echo "$latest_request" | jq -r '.requested_at')
-    justification=$(echo "$latest_request" | jq -r '.justification')
+  decision_owners=$(echo "$item" | jq -r '.decision_owners | join(", ")')
 
+  latest_request=$(echo "$item" | jq -r '.requesters | sort_by(.requested_at) | last')
+  requested_at=$(echo "$latest_request" | jq -r '.requested_at')
+  justification=$(echo "$latest_request" | jq -r '.justification')
+
+  # echo "REPO_REMOTE: ${RT_REPO_REMOTE}    repo_key: ${repo_key} "
+  if [[ ("${RT_REMOTE}" == "${repo_key}") ]] ; then
     echo " |  ${waiver_id} | ${pkg_name} | ${pkg_version} | ${requested_at} | ${justification} | ${decision_owners} | "
+  fi
 done
 
 
